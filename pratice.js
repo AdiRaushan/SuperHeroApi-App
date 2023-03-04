710e78d7e1e0ba81ef520f83f05881155ae1fc00
adiraushan <adiraushan2k@gmail.com>
2023-03-04 23:23:55 +0530

    commit with picture

--- pratice.js
+++ pratice.js
@@ -3,17 +3,16 @@
 // a superhero fuction
 
 const SUPERHERO_Token = '3512451342333137'
-const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_Token}`
+const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_Token}` 
 
 const getSuperhero  = (id, name) => {
     fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
-    .then(json => console.log(json.image.url))
-}
+    .then(json => {
+        console.log(json)
+        document.querySelector('body').innerHTML += `<img src= "${json.image.url}" height="200" width="200"/>`
+    })
 
+    }
 
-getSuperhero(25)
-
-const img = "https://www.superherodb.com/pictures2/portraits/10/100/10405.jpg"
-
-document.querySelector('body').innerHTML += `<img src= "${img}"/>`
+getSuperhero(60)
