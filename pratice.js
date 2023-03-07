cfd6194ad4a4080e1723d5c0222170904eb98c05
adiraushan <adiraushan2k@gmail.com>
2023-03-07 13:43:14 +0530

    1

--- pratice.js
+++ pratice.js
@@ -5,6 +5,8 @@
 const SUPERHERO_Token = '3512451342333137'
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_Token}` 
 
+const newheroButton = document.getElementById('heroButton')
+
 const getSuperhero  = (id, name) => {
     fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
