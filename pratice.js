e8d98282e338612ecf38c103b6a37d869927f600
adiraushan <adiraushan2k@gmail.com>
2023-03-07 14:15:22 +0530

    2

--- pratice.js
+++ pratice.js
@@ -6,15 +6,32 @@ const SUPERHERO_Token = '3512451342333137'
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_Token}` 
 
 const newheroButton = document.getElementById('heroButton')
+const newimgdiv = document.getElementById('heroImg')
 
-const getSuperhero  = (id, name) => {
+const getRandomSuperhero  = (id, name) => {
+
+
     fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
     .then(json => {
         console.log(json)
-        document.querySelector('body').innerHTML += `<img src= "${json.image.url}" height="200" width="200"/>`
+        newimgdiv.innerHTML = `<img src= "${json.image.url}" height="200" width="200"/>`
     })
 
-    }
+}
 
-getSuperhero(60)
+const getsearchSuperhero = () => {
+    fetch(`{BASE_URL}/search/${name}`)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+        newimgdiv.innerHTML = `<img src= "${json.image.url}" height="200" width="200"/>`
+    })
+}
+
+const randomHero = () => {
+    const numberofHero = 731
+    return Math.floor(Math.random() * numberofHero) + 1
+}
+ 
+newheroButton.onclick = () => getRandomSuperhero(randomHero())
