c4a9d55fe236f5ed15e9978f599b6be0c467d4d5
adiraushan <adiraushan2k@gmail.com>
2023-03-07 17:05:59 +0530

    5-one

--- pratice.js
+++ pratice.js
@@ -8,6 +8,7 @@ const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_Token}`
 const newheroButton = document.getElementById('heroButton')
 const newimgdiv = document.getElementById('heroImg')
 const SearchButton = document.getElementById('searchBtn')
+const inputsearch = document.getElementById('SearchInput')
 
 const getRandomSuperhero  = (id) => {
 
@@ -15,18 +16,22 @@ const getRandomSuperhero  = (id) => {
     fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
     .then(json => {
-        console.log(json)
-        newimgdiv.innerHTML = `<img src= "${json.image.url}" height="200" width="200"/>`
+        const name = `<h2> ${json.name}</h2>`
+        const power = `<h3> COMBAT: ${json.powerstats.combat}</h3>`
+        // console.log(json)
+        newimgdiv.innerHTML = `${name}<img src= "${json.image.url}" height="200" width="200"/>
+        ${power}`
     })
 
 }
 
 const getsearchSuperhero = (name) => {
+    console.log(inputsearch.value)
     fetch(`${BASE_URL}/search/${name}`)
     .then(response => response.json())
     .then(json => {
         const hero = json.results[0]
-        console.log(hero)
+        // console.log(hero)
         newimgdiv.innerHTML = `<img src= "${hero.image.url}" height="200" width="200"/>`
     })
 }
@@ -38,4 +43,4 @@ const randomHero = () => {
  
 newheroButton.onclick = () => getRandomSuperhero(randomHero())
 
-SearchButton.onclick = () => getsearchSuperhero('spider')
+SearchButton.onclick = () => getsearchSuperhero(inputsearch.value)
