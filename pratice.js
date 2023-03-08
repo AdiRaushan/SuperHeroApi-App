b23c2fa70d3a7d55136da206e5d3c557062ccf57
adiraushan <adiraushan2k@gmail.com>
2023-03-08 18:02:49 +0530

    final

--- pratice.js
+++ pratice.js
@@ -16,11 +16,7 @@ const getRandomSuperhero  = (id) => {
     fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
     .then(json => {
-        const stats = getstats(json)
-        const name = `<h2> ${json.name}</h2>`
-                // console.log(json)
-        newimgdiv.innerHTML = `${name}<img src= "${json.image.url}" height="200" width="200"/>
-        ${stats}`
+        getstats(json)
     })
 
 }
@@ -40,12 +36,14 @@ const stattoemoji = {
 }
 
 const getstats = (character) =>{
+    const name = `<h1> ${character.name}</h1>`
+   const img =  `<img src= "${character.image.url}" height="500" width="500" />`
    const stats = Object.keys(character.powerstats).map(stat => {
         return  `<p>${stattoemoji[stat]} ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
-    })
+    }).join('')
     console.log(stats)
+    newimgdiv.innerHTML = `${name}${img}${stats}`
     
-    return stats.join('')
 }
 
 const getsearchSuperhero = (name) => {
@@ -54,11 +52,9 @@ const getsearchSuperhero = (name) => {
     .then(response => response.json())
     .then(json => {
         const hero = json.results[0]
-        // console.log(hero)
-        newimgdiv.innerHTML = `<img src= "${hero.image.url}" height="200" width="200"/>`
+        getstats(hero)
     })
 }
-
 const randomHero = () => {
     const numberofHero = 731
     return Math.floor(Math.random() * numberofHero) + 1
